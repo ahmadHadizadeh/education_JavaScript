b1740249f08db1a5bcf07564ebc91bdbc5b6e070
ahmad hadizadeh <giv.m@yahoo.com>
2023-04-05 13:11:32 +0430

    string Method js

---
+++ stringMethod.js
@@ -0,0 +1,66 @@
+// const car = { carName: "Volvo", speed: 260, model: 1990, color: "lightBlue" }
+// console.log(car.carName);
+// console.log(car.speed);
+// console.log(car.model);
+// console.log(typeof (car));
+// console.log(car["carName"]);
+// console.log(typeof (function () { }));
+
+// const user = {
+//     firstName: "ahmad",
+//     lastName: "hadizadeh",
+//     age: 20,
+//     car:"volvo",
+//     fullName: function () {
+//         return this.firstName +" "+ this.lastName;
+//     }
+// }
+// console.log(user);
+// x = new String();
+// y = new Number();
+// b = new Boolean();
+
+// console.log(typeof(y));
+// console.log(typeof (x));
+// console.log(typeof (b));
+// console.log(Date());
+// let x = "maryam";
+// let y = 1;
+// console.log(x==y);
+
+// let text = "Apple, Banana, Kiwi";
+// console.log(text.slice(7,13));
+// console.log(text.substring(2,8));
+// console.log(text.substring(0));
+
+// replace()==> js
+
+// let text = "Please Visit Microsoft! IS very Big company";
+// var rp = text.replace(/MICROSOFT/i, "Google");==>tolower()
+// text = text.replaceAll("visit", "check");
+// text = text.replaceAll("big", "small");
+// console.log(text);
+// let upr = text.toUpperCase();
+// let lw = text.toLowerCase();
+// console.log(upr);
+// console.log(lw);
+let text = "hello";
+// var tm = text.trim();
+
+// var tm = text.trimStart();
+//  console.log(tm);
+// var end = text.trimEnd();
+// console.log(end.length);
+
+// var t = text.concat("ahmad");
+// var arry1 = ["A", "B","c"];
+// var arry2 = ["F", "X","Z"];
+
+// var g = arry1.concat(arry2+ "ahmad");
+// console.log(g);
+var txt = "ahmad hadizadeh";
+// var x = txt.trim().toLocaleUpperCase().charAt(7);
+// console.log(x);
+var x = "ahmad";
+var t = x.split(",");
+console.log(t);
